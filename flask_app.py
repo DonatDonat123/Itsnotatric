96ed5f400806ede8e0129546b198e26d228801ca
DonatDonat123 <dennis.h.doerrich@gmail.com>
2018-07-15 15:58:25 +0200

    testing ssh connection

--- flask_app.py
+++ flask_app.py
@@ -83,6 +83,7 @@ def chatbot():
         db.session.add(entry2)
         db.session.commit()
         db.session.close()
+        print ('TEST')
         #return render_template("carnovo.html", results=Dealers.query.all())
         return redirect(url_for('chatbot'))
     else:
