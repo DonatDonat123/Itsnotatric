843b8c2d27beeeddae9654585eb8bb5a385591a9
DonatDonat123 <dennis.h.doerrich@gmail.com>
2018-09-11 17:39:16 +0200

    add functionality for comparing 2 cities and set_origin

--- nlp_nltk.py
+++ nlp_nltk.py
@@ -17,6 +17,7 @@ import nltk
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
+from strings.city_list import CITIES
 
 STANFORD_JAR_FILE = './stanford-ner/stanford-ner.jar'
 STANFORD_ENG_FILE = './stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz'
@@ -40,6 +41,11 @@ RESPONSE_FOUND_2_CITIES = "You want to compare {} with {}, correct ?"
 
 CONFIRMATIONS_SET = {"yes", "y", "yep", "ok", "correct"}
 
+DESCRIBE_WHEATHER_1_CITY = "First of all, let's talk about the weather in {}. The temperature will be {} °C tomorrow"
+DESCRIBE_WHEATHER_2_CITIES = "In {} it will be {} °C and in {} {} °C tomorrow"
+
+ASK_FOR_INSTRUCTIONS = "Sorry for that. Please ask me again in other words."
+
 def random_answer():
     return random.choice(GENERAL_RESPONSES)
 
@@ -53,32 +59,79 @@ def give_random_greeting():
 def _check_for_confirmation(sentence):
     return any(confirmation in sentence for confirmation in CONFIRMATIONS_SET)
 
+def words_in_city_list(tokenized_text):
+    cities_found = []
+    for word in tokenized_text:
+        if word in CITIES:
+            cities_found.append(word)
+    return cities_found
+
+def get_wheather(city):
+    fc1 = owm.daily_forecast(city)
+    tomorrow = pyowm.timeutils.tomorrow()
+    sunny = fc1.will_be_sunny_at(tomorrow)
+    weather1 = fc1.get_weather_at(tomorrow)
+    temperature = np.round(weather1.get_temperature(unit='celsius')["day"]).astype(int)
+    return sunny, temperature
+
 class NLP:
     def __init__(self):
         print("NLP class was created")
-        self.reset_all_config_variables()
+        self._reset_all_config_variables()
         self.tagger = StanfordNERTagger(STANFORD_ENG_FILE, STANFORD_JAR_FILE, encoding='utf-8')
 
-    def reset_all_config_variables(self, verbose = False):
-        self.city1 = None
-        self.city2 = None
-        self.many_cities = None
-        self.origin = None
-        self.blindshot = False
-        self.need_confirmation_1_city = False
-        self.need_confirmation_2_cities = False
-        self.need_confirmation_many_cities = False
+    def _reset_all_config_variables(self,verbose = False):
+        self.reset_config_variables(all_except_origin=True, origin=True, verbose=verbose)
+
+    def reset_config_variables(self, all_except_origin = False, city1 = False, city2 = False, many_cities = False,
+                               blindshot = False, confirmations = False, ask_for_origin = False, origin = False, verbose = False):
+        if city1 or all_except_origin:
+            self.city1 = None
+        if city2 or all_except_origin:
+            self.city2 = None
+        if many_cities or all_except_origin:
+            self.many_cities = None
+        if blindshot or all_except_origin:
+            self.blindshot = False
+        if confirmations or all_except_origin:
+            self.need_confirmation_1_city = False
+            self.need_confirmation_2_cities = False
+        if ask_for_origin or all:
+            self.ask_for_origin = False
+        if origin:
+            self.origin = None
         if verbose:
             return "Everything reset, what do you want to do now ?"
+
+    def set_origin(self, text):
+        cities = self._find_cities(text)
+        if len(cities) == 0:
+            return "I don't know this city, again please."
+        elif len(cities) == 1:
+            self.ask_for_origin = False
+            self.origin = cities[0]
+            return "origin set to {}".format(cities[0])
+        elif len(cities) > 1:
+            return self.found_many_cities(1)
+
+    def ask_for_instructions(self):
+        self.reset_config_variables(all_except_origin=True)
+        return ASK_FOR_INSTRUCTIONS
 
     def introduction(self):
         return INTRODUCTION
 
-    def check_for_cities(self, text):
+    def _find_cities(self, text):
         tokenized_text = word_tokenize(text)
         tagged_text = np.array(self.tagger.tag(tokenized_text))
-        print ("tagged_text: {}".format(tagged_text))
-        cities = tagged_text[tagged_text[:, 1] == 'LOCATION'][:, 0]
+        print("tagged_text: {}".format(tagged_text))
+        cities = set(tagged_text[tagged_text[:, 1] == 'LOCATION'][:, 0])
+        cities2 = words_in_city_list(tokenized_text)
+        cities.update(cities2)
+        return list(cities)
+
+    def check_for_cities(self, text):
+        cities = self._find_cities(text)
         if len(cities) == 0:
             return False
         elif len(cities) == 1:
@@ -92,51 +145,74 @@ class NLP:
             return True
         elif len(cities) > 2:
             self.many_cities = cities
-            self.need_confirmation_many_cities = True
             return True
 
     def found_1_city(self):
+        self.need_confirmation_1_city = True
         return RESPONSE_FOUND_1_CITY.format(self.city1)
 
     def found_2_cities(self):
+        self.need_confirmation_2_cities = True
         return RESPONSE_FOUND_2_CITIES.format(self.city1, self.city2)
 
-    def found_many_cities(self):
+    def found_many_cities(self, number_cities_wanted):
         response = "I detected: "
-        for city in self.many_cities:
-            response.append("{}, ".format(city))
-        response.append(". Please decide for 2 cities")
+        response += "{}".format(self.many_cities[0])
+        for city in self.many_cities[1:]:
+            response += (", {}".format(city))
+        response += (". Please decide for {} cities".format(number_cities_wanted))
         return response
 
     def describe_one_city(self):
+        sunny1, temp1 = get_wheather(self.city1)
+        return DESCRIBE_WHEATHER_1_CITY.format(self.city1, temp1)
 
-        fc1 = owm.daily_forecast(self.city1)
-        tomorrow = pyowm.timeutils.tomorrow()
-        sunny1 = fc1.will_be_sunny_at(tomorrow)
-        weather1 = fc1.get_weather_at(tomorrow)
-        temp1 = weather1.get_temperature(unit='celsius')["day"]
+    def compare_two_cities(self):
+        sunny1, temp1 = get_wheather(self.city1)
+        sunny2, temp2 = get_wheather(self.city2)
+        return DESCRIBE_WHEATHER_2_CITIES.format(self.city1, temp1, self.city2, temp2)
 
-        response = "First of all, let's talk about the weather in {}. The temperature will be {} tomorrow"\
-            .       format(self.city1, temp1)
 
-        return response
+    def confirm_city(self, number_cities, sentence):
+        self.reset_config_variables(confirmations=True)
+        if _check_for_confirmation(sentence):
+            if number_cities == 1:
+                return self.describe_one_city()
+            elif number_cities == 2:
+                return self.compare_two_cities()
+            else: raise ValueError("can only confirm for 1 or 2 cities")
+        else:
+            return self.ask_for_instructions()
 
     def respond(self, sentence):
         print('input message = {}'.format(sentence))
         resp = None
         if any(sentence in s for s in ("start", "\start")):
-            return self.introduction();
+            self._reset_all_config_variables(verbose=False)
+            self.ask_for_origin = True
+            response = self.introduction();
         elif any(sentence in s for s in ("reset", "cancel")):
-            return self.reset_all_config_variables(verbose=True);
+            response = self._reset_all_config_variables(verbose=True)
+        elif self.ask_for_origin:
+            response = self.set_origin(sentence)
         elif self.need_confirmation_1_city:
-            if _check_for_confirmation(sentence):
-                return self.describe_one_city()
+            response = self.confirm_city(1, sentence)
+        elif self.need_confirmation_2_cities:
+            response = self.confirm_city(2, sentence)
         elif check_for_greeting(sentence):
-            return give_random_greeting()
+            response = give_random_greeting()
         elif self.check_for_cities(sentence):
-            if self.many_cities: resp = self.found_many_cities()
-            elif self.city1 and self.city2: resp = self.found_2_cities()
-            elif self.city1: resp = self.found_1_city()
+            if self.many_cities is not None: response = self.found_many_cities(2)
+            elif self.city1 and self.city2: response = self.found_2_cities()
+            elif self.city1: response = self.found_1_city()
         else:
-            resp = random_answer();
-        return resp
+            response = random_answer();
+        print ("response = {}".format(response))
+        return response
+
+if __name__ == "__main__":
+    nlp = NLP()
+    nlp.respond("start")
+    nlp.respond("Berlin")
+    nlp.respond("Tell me about Berlin and Karlsruhe")
+    nlp.respond("yes")
